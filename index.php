054018e62f8f782a518e722709c4630cb7c25238
MonicaAuditore <mauditore@live.it>
2023-02-20 14:43:19 +0100

    add_tabella senza stile

--- index.php
+++ index.php
@@ -48,16 +48,45 @@ $hotels = [
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Hotel</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
 </head>
 <body>
-<?php foreach ($hotels as $hotel): ?>
+<!-- <?php foreach ($hotels as $hotel): ?>
 		<h2><?= $hotel['name'] ?></h2>
 		<p><?= $hotel['description'] ?></p>
 		<p>Parcheggio: <?= $hotel['parking'] ? 'Si' : 'No' ?></p>
 		<p>Voto: <?= $hotel['vote'] ?>/5</p>
 		<p>Distanza dal centro: <?= $hotel['distance_to_center'] ?> km</p>
 		<hr>
-	<?php endforeach; ?>
+	<?php endforeach; ?> -->
+
+  <div class="container">
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Descrizione</th>
+					<th>Parcheggio</th>
+					<th>Voto</th>
+					<th>Distanza dal centro</th>
+				</tr>
+			</thead>
+			<tbody class="table-striped">
+				<?php foreach ($hotels as $hotel): ?>
+					<tr>
+						<td><?= $hotel['name'] ?></td>
+						<td><?= $hotel['description'] ?></td>
+						<td><?= $hotel['parking'] ? 'Si' : 'No' ?></td>
+						<td><?= $hotel['vote'] ?>/5</td>
+						<td><?= $hotel['distance_to_center'] ?> km</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
